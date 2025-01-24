c39bdb52f759a4782b8b97f75808c6eb338093ac
Szymon Kołodziejski <koodziejskisz@outlook.com>
2025-01-24 23:14:35 +0100

    feat: add usage of model deployment

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 from src.preprocessing import DataPreprocessing
 from src.feature_engineering import FeatureEngineering
 from src.training import ModelTraining
-from src.evaluation import ModelEvaluation
 from src.deployment import ModelDeployment
-from src.monitoring import Monitoring
 from src.mlpipeline import MLPipeFlow
 
 
 def main():
     components = [
-        DataPreprocessing('data.csv'),
-        FeatureEngineering(),
-        ModelTraining(),
-        ModelEvaluation('model.pkl'),
-        ModelDeployment('model.pkl', 'example-model/model.pkl'),
-        Monitoring('model.pkl')
+        DataPreprocessing(data_path='test_data/exampleData_TCGA_LUAD_2000.csv'),
+        FeatureEngineering(target='class'),
+        ModelTraining(model_name='linear_regression'),
+        ModelDeployment(model_path='model_example/model.pkl'),
     ]
 
     pipeline = MLPipeFlow(components)
