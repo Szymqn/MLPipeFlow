b1669633a4a33cce1d8cd149580e63e629aa4775
Szymon Kołodziejski <koodziejskisz@outlook.com>
2025-01-29 22:16:16 +0100

    feat: update model & add more metrics

--- src/monitoring.py
+++ src/monitoring.py
@@ -1,4 +1,7 @@
 from abc import ABC
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
 from src.pipeline import PipelineComponent
 from pickle import load
 
@@ -9,14 +12,27 @@ class Monitoring(PipelineComponent, ABC):
 
     def execute(self, model_path, data):
         print('Monitoring model...')
-        print("Model path: ", model_path)
-        print("Data:", data)
         with open(model_path, 'rb') as f:
             model = load(f)
-        print("Model: ", model)
         features, target = data
         if hasattr(model, 'predict'):
             predictions = model.predict(features)
-            return predictions
         else:
             raise AttributeError(f"Loaded object from {self.model_path} does not have a 'predict' method.")
+
+        acc_score = accuracy_score(target, predictions)
+        precision = precision_score(target, predictions, average='binary')
+        recall = recall_score(target, predictions, average='binary')
+        f1 = f1_score(target, predictions, average='binary')
+
+        print("ACC: ", acc_score)
+        print("Precision: ", precision)
+        print("Recall: ", recall)
+        print("F1 Score: ", f1)
+
+        return {
+            'accuracy': acc_score,
+            'precision': precision,
+            'recall': recall,
+            'f1_score': f1
+        }
