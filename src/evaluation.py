bab01bbaadfa0362b8e757e39557aab6f2efe310
Szymon Kołodziejski <koodziejskisz@outlook.com>
2025-01-29 22:37:24 +0100

    feat: add more metrics

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,15 +1,30 @@
 from abc import ABC
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from src.pipeline import PipelineComponent
-from sklearn.metrics import accuracy_score
 
 
 class ModelEvaluation(PipelineComponent, ABC):
     def __init__(self):
         pass
 
-    def execute(self, data, model):
+    def execute(self, model, data):
         print('Evaluating model...')
         features, target = data
         predictions = model.predict(features)
-        print("ACC: ", accuracy_score(target, predictions))
-        return data
+
+        acc_score = accuracy_score(target, predictions)
+        precision = precision_score(target, predictions, average='binary')
+        recall = recall_score(target, predictions, average='binary')
+        f1 = f1_score(target, predictions, average='binary')
+
+        print("ACC: ", acc_score)
+        print("Precision: ", precision)
+        print("Recall: ", recall)
+        print("F1 Score: ", f1)
+
+        return {
+            'accuracy': acc_score,
+            'precision': precision,
+            'recall': recall,
+            'f1_score': f1
+        }
